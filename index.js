0cdd4765c2cd1eefe62da5805289975fd666203e
Michael Gamarra <Mgamarra908@gmail.com>
2023-09-22 20:23:40 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+        return fetch('http://localhost:3000/users', {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          body: JSON.stringify({ name, email })
+        })
+        .then(resp => resp.json())
+        .then(data => {
+          console.log(data);
+          const userId = data.id;
+            const userIdElement = document.createElement('p');
+            userIdElement.textContent = `User ID: ${userId}`;
+            document.body.appendChild(userIdElement);
+  })
+        
+  .catch(function (error) {
+    const errorMessageElement = document.createElement('p');
+    errorMessageElement.textContent = `Error: ${error}`;
+    document.body.appendChild(errorMessageElement);
+    });
+ }
